99a04de618c4d7b2c0709990d6b5ae0b5e9e4c2e
Srinivas <abcom@Abcoms-MacBook-Air.local>
2025-01-26 19:44:48 +0530

    changes inthe page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -11,7 +11,7 @@ const Layout = (props: any) => {
       className={` min-h-screen h-full w-screen overflow-x-hidden flex flex-col text-white items-center font-inter 
     `}
     >
-      <div className=" fixed top-[75%] right-0  z-[1000] ">
+      {/* <div className=" fixed top-[75%] right-0  z-[1000] ">
         <a
           href="https://api.whatsapp.com/send/?phone=919948348899&text=I%27m+interested+to+work+with+team+TrinityXr"
           target="_blank"
@@ -22,7 +22,7 @@ const Layout = (props: any) => {
             className=" rounded-l-full md:py-2 md:pl-2 pl-1 py-1  bg-white md:h-24 h-12  "
           />
         </a>
-      </div>
+      </div> */}
       <Navbar />
       <div className=" flex-grow w-full mx-auto  relative ">
         {outlet}{" "}
